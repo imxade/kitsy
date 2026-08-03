7d0903a1f6964bf931578ae671d898fff61c5458
Rituraj <56511165+imxade@users.noreply.github.com>
2026-08-03 19:22:45 +0530

    Nixhost (#6)
    * Update devShells in flake.nix
    
    Removed 'pkgs.act' from buildInputs and added npm build and start commands to shellHook.
    
    * nixhost config
    
    * build: add NixHost deployment package
    
    * test: verify automatic NixHost redeployment
    
    ---------
    
    Co-authored-by: NixHost acceptance test <nixhost-test@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -1,26 +1,48 @@
 {
-  description = "My dev shell";
+  description = "Kitsy development and NixHost deployment";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.nodejs_24
-        pkgs.act
-        pkgs.zsh
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
       ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = import ./nixhost.nix { inherit pkgs; };
+        }
+      );
 
-      shellHook = ''
-        npm install
-        node -v
-        npm -v
-        zsh
-      '';
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/kitsy";
+        };
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_24
+            ];
+          };
+        }
+      );
     };
-  };
 }
